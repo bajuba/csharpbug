0e28ffe17d9b5914fce666376e6a59a0d635fe5b
jmunger <jmunger@lakelandcollege.edu>
2019-02-08 15:06:28 -0600

    removed writeline

--- BugCrawl/Program.cs
+++ BugCrawl/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Timers;
 
 namespace BugCrawl
 {
@@ -6,7 +7,26 @@ namespace BugCrawl
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            
+        var aTimer = new Timer();
+        aTimer.Interval = 50;
+
+        // Hook up the Elapsed event for the timer. 
+        aTimer.Elapsed += render;
+
+        // Have the timer fire repeated events (true is the default)
+        aTimer.AutoReset = true;
+
+        // Start the timer
+        aTimer.Enabled = true;
+
+       
+        }
+        private static void render(Object source, System.Timers.ElapsedEventArgs e)
+        {
+            //Console.Clear();
+            Console.WriteLine("hello");
         }
     }
 }
