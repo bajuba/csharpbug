7bb97fbe51a0adebe7aa8269a925b59ea2b1b3eb
Nick Miller <6905813@gmail.com>
2019-02-11 16:14:08 -0600

    call myWorld draw on interval

--- BugCrawl/Program.cs
+++ BugCrawl/Program.cs
@@ -1,33 +1,38 @@
 ﻿using System;
 using System.Timers;
-
+// https://stackoverflow.com/questions/9977393/how-do-i-pass-an-object-into-a-timer-event
 namespace BugCrawl
 {
     class Program
     {
+        private static Timer aTimer;
         static void Main(string[] args)
         {
         World myWorld = new World("bob");
         Console.WriteLine(myWorld.draw());
         Console.WriteLine(myWorld.sayName());
-        var aTimer = new Timer();
-        aTimer.Interval = 50;
+        aTimer = new System.Timers.Timer();
+        aTimer.Interval = 5000;
 
         // Hook up the Elapsed event for the timer. 
-        aTimer.Elapsed += render;
+        // aTimer.Elapsed += render;
+        aTimer.Elapsed += (sender, e) => render(sender, e, myWorld);
 
         // Have the timer fire repeated events (true is the default)
         aTimer.AutoReset = true;
 
         // Start the timer
         aTimer.Enabled = true;
-
+      Console.WriteLine("Press the Enter key to exit anytime... ");
+      Console.ReadLine();
 
         }
-        private static void render(Object source, System.Timers.ElapsedEventArgs e)
+        static void render(Object source, System.Timers.ElapsedEventArgs e, World myWorld)
         {
             //Console.Clear();
-            Console.WriteLine("hello");
+            // Console.WriteLine("hello");
+        Console.WriteLine("Raised: {0}", e.SignalTime);
+        Console.WriteLine(myWorld.draw());
         }
     }
 }
