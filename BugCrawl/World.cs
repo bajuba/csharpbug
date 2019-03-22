72b16f378018595b6988a1a7bf810bc2dd0a3e2c
jmunger <jmunger@lakelandcollege.edu>
2019-03-22 15:09:36 -0500

    get logan to appear

--- BugCrawl/World.cs
+++ BugCrawl/World.cs
@@ -61,19 +61,21 @@ namespace BugCrawl
                 //surfaceLevel[guy.xPos+guy.yPos*width] = guy.dash;
                 surfaceLevel[guy.xPos + guy.yPos * width] = guy.body;
             }
-            for (int x = 0; x < 10; x++)
+            for (int x = 0; x < width; x++)
             {
-                for (int y = 0; y < 10; y++)
+                for (int y = 0; y < width; y++)
                 {
                     if (skyLevel[x + y * width] == "")
+                    {
                         if (groundLevel[x + y * width] == "")
                             toDraw += surfaceLevel[x + y * width];
                         else
                             toDraw += groundLevel[x + y * width];
+                    }
                     else
                         toDraw += skyLevel[x + y * width];
 
-                    toDraw += groundLevel[x + y * width];
+                    //toDraw += groundLevel[x + y * width];
                     //toDraw= toDraw + stage[i][j];
                 }
                 toDraw += "\n";
