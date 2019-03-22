34b5f4cac9fa4ede151954cb701851b91c8e7a35
jmunger <jmunger@lakelandcollege.edu>
2019-03-22 15:05:46 -0500

    finding the draw bug

--- BugCrawl/World.cs
+++ BugCrawl/World.cs
@@ -16,68 +16,70 @@ namespace BugCrawl
         public List<Creature> stuff;
         const int width = 10;
 
-        public World(string inputName){
+        public World(string inputName)
+        {
             groundLevel = new List<string>();
-            for(int x=0;x<width;x++)
+            for (int x = 0; x < width; x++)
             {
-               for(int y=0;y<width;y++)
-               {
-                   //this.stage[x+y*width] = "-";
-                   this.groundLevel.Add("-");
-               }
+                for (int y = 0; y < width; y++)
+                {
+                    //this.stage[x+y*width] = "-";
+                    this.groundLevel.Add("-");
+                }
             }
 
             surfaceLevel = new List<string>();
-            for(int x=0;x<width;x++)
+            for (int x = 0; x < width; x++)
             {
-               for(int y=0;y<width;y++)
-               {
-                   //this.stage[x+y*width] = "-";
-                   this.surfaceLevel.Add("");
-               }
+                for (int y = 0; y < width; y++)
+                {
+                    //this.stage[x+y*width] = "-";
+                    this.surfaceLevel.Add("");
+                }
             }
 
             skyLevel = new List<string>();
-            for(int x=0;x<width;x++)
+            for (int x = 0; x < width; x++)
             {
-               for(int y=0;y<width;y++)
-               {
-                   //this.stage[x+y*width] = "-";
-                   this.skyLevel.Add("");
-               }
+                for (int y = 0; y < width; y++)
+                {
+                    //this.stage[x+y*width] = "-";
+                    this.skyLevel.Add("");
+                }
             }
 
             this.name = inputName;
             stuff = new List<Creature>();
         }
 
-        public string draw(){
-            string toDraw="";
-            foreach(Creature guy in this.stuff)
+        public string draw()
+        {
+            string toDraw = "";
+            foreach (Creature guy in this.stuff)
             {
                 //x+y*width
                 //surfaceLevel[guy.xPos+guy.yPos*width] = guy.dash;
-                surfaceLevel[guy.xPos+guy.yPos*width] = guy.body;  
+                surfaceLevel[guy.xPos + guy.yPos * width] = guy.body;
             }
-            for(int x=0;x<10;x++)
+            for (int x = 0; x < 10; x++)
             {
-               for(int y=0;y<10;y++)
-               {
-                   if(skyLevel[x+y*width] == "")
-                    if(groundLevel[x+y*width] == "")
-                        toDraw+=surfaceLevel[x+y*width];
+                for (int y = 0; y < 10; y++)
+                {
+                    if (skyLevel[x + y * width] == "")
+                        if (groundLevel[x + y * width] == "")
+                            toDraw += surfaceLevel[x + y * width];
+                        else
+                            toDraw += groundLevel[x + y * width];
                     else
-                        toDraw+=groundLevel[x+y*width];
-                    else
-                        toDraw+=skyLevel[x+y*width];
+                        toDraw += skyLevel[x + y * width];
 
-                   toDraw+=groundLevel[x+y*width];
-                   //toDraw= toDraw + stage[i][j];
-               }
-               toDraw += "\n";
+                    toDraw += groundLevel[x + y * width];
+                    //toDraw= toDraw + stage[i][j];
+                }
+                toDraw += "\n";
             }
 
-            
+
 
             return toDraw;
         }
